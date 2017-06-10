580e62f2730b424cec0d8b682b971687b4c23cbf
MicahMonfrini <ffrock905@gmail.com>
2017-06-10 16:47:07 -0500

    add logic to map over books and render

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
 
 class BooksPage extends Component {
   constructor(props) {
@@ -7,23 +8,50 @@ class BooksPage extends Component {
   }
 
   componentDidMount() {
-
+    // added this so next.js wouldn't throw "getInitialProps" error
   }
 
   render() {
+    // test array for map function
+    const books = [
+      {
+        title: "Sweet Book",
+        author: "Dude Duderson",
+        category: "Non-fiction"
+      },
+      {
+        title: "Sweet Book",
+        author: "Dude Duderson",
+        category: "Non-fiction"
+      }
+    ];
+    // map over books array. will pass to component via props
+    const bookList = books.map((book, index) => {
+      return (
+        <ul key={index}>
+          <li><strong>Title:</strong> {book.title}</li>
+          <li><strong>Author:</strong> {book.author}</li>
+          <li><strong>Category:</strong> {book.category}</li>
+          {/* need to configure url to generate unique title for detail page */}
+          <Link href="/books/:title">
+            <a>More info</a>
+          </Link>
+          <br />
+          <button>Delete</button>
+        </ul>
+      );
+    });
     return (
-      <div></div>
+      <div>
+        {bookList}
+      </div>
     );
   }
 }
 
+// prop types
 BooksPage.propTypes = {
   books: PropTypes.array.isRequired
 };
 
 export default BooksPage;
-
-
-// const bookList = this.props.books.map((user, index) => {
-//   return
-// })
