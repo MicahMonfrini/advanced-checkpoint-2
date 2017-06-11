6978ed1bdf7b437f8f8388c5badbbcc8dffaa86d
MicahMonfrini <ffrock905@gmail.com>
2017-06-10 21:50:10 -0500

    clean up BookRouter

--- src/routers/BookRouter.js
+++ src/routers/BookRouter.js
@@ -2,26 +2,14 @@ import express from "express";
 import BookController from "../controllers/BookController";
 
 const router = express.Router();
-// @FIXME: remove disabling of rules
-/* eslint-disable no-unused-vars */
-
-// Lists all books
 
 router.get("/api/books", BookController.list);
 
-// Shows single book
-
 router.get("/api/books/:id", BookController.show);
-
-// Creates single book
 
 router.post("/api/books/:id", BookController.create);
 
-// Deletes single book
-
 router.delete("/api/books/:id", BookController.delete);
-
-// Updates single book
 
 router.put("/api/books/:id", BookController.update);
 
