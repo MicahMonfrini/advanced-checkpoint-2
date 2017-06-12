efd71d81e8b88cee14b4f79796889f793491f290
MicahMonfrini <ffrock905@gmail.com>
2017-06-11 21:00:20 -0500

    add validation to bookSchema

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,11 +1,31 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  date: Number,
-  publisher: String,
-  category: String
+  title: {
+    type: String,
+    maxlength: [ 100, "Maximum character limit reached. Please try again." ],
+    required: true
+  },
+  author: {
+    type: String,
+    maxlength: [ 50, "Maximum character limit reached. Please try again." ],
+    required: true
+  },
+  date: {
+    type: Number,
+    max: [ 100, "Maximum character limit reached. Please try again." ],
+    required: true
+  },
+  publisher: {
+    type: String,
+    maxlength: [ 128, "Maximum character limit reached. Please try again." ],
+    required: true
+  },
+  category: {
+    type: String,
+    maxlength: [ 128, "Maximum character limit reached. Please try again." ],
+    required: true
+  }
 });
 
 export default mongoose.model("Book", bookSchema);
