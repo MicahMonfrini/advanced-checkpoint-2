a7f0b5b4a927214b8e51fa4cdb5a879e59547942
MicahMonfrini <ffrock905@gmail.com>
2017-06-14 18:13:58 -0500

    add createBook action

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,16 +3,28 @@ export const BOOKS_LOADED = "BOOKS_LOADED";
 export function loadBooks() {
   return function (dispatch) {
     fetch("/api/books")
-    .then(response => {
-      return response.json();
-    }).then(books => {
-      dispatch(booksLoaded(books));
-    });
+      .then(response => {
+        return response.json();
+      })
+      .then(books => {
+        dispatch(booksLoaded(books));
+      });
   };
 }
 function booksLoaded(books) {
   return {
     type: BOOKS_LOADED,
     value: books
+  };
+}
+
+export function createBook(book) {
+  return function (dispatch) {
+    fetch("/api/books", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(book)
+    })
+      .then(() => dispatch(loadBooks()));
   };
 }
