a3984a49935c52627900293134e00aaca07021ac
MicahMonfrini <ffrock905@gmail.com>
2017-06-19 18:08:29 -0500

    convert bookdetail to class component

--- src/pages/bookdetail.js
+++ src/pages/bookdetail.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {Component} from "react";
 import { initStore } from "../store";
 import withRedux from "next-redux-wrapper";
 
-function BookDetail() {
-  return (
-    <div>
-      "A specific book"
-    </div>
-  );
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>
+        "A specific book"
+      </div>
+    );
+  }
 }
 
-export default withRedux(initStore, null, null)(BookDetail)
+export default withRedux(initStore, null, null)(BookDetail);
